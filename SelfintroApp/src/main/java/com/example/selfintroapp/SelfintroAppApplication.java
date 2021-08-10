8519f0eca7147ea10edb05d4ea65d74b5464de76
Keisuke-yunoki <59494597+Keisuke-yunoki@users.noreply.github.com>
2021-08-10 14:50:30 +0900

    新規プロジェクトの白紙状態

---
+++ SelfintroApp/src/main/java/com/example/selfintroapp/SelfintroAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.selfintroapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfintroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfintroAppApplication.class, args);
+	}
+
+}
---
+++ SelfintroApp/src/test/java/com/example/selfintroapp/SelfintroAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selfintroapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfintroAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
