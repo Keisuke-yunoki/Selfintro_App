a62590e7dafa3b7ed66e50076c6e9d17010a476a
Keisuke-yunoki <59494597+Keisuke-yunoki@users.noreply.github.com>
2021-09-19 21:19:11 +0900

    SelfintroAppApplication.javaに＠MapperScan を追加。

--- SelfintroApp/src/main/java/com/example/selfintroapp/SelfintroAppApplication.java
+++ SelfintroApp/src/main/java/com/example/selfintroapp/SelfintroAppApplication.java
@@ -1,9 +1,11 @@
 package com.example.selfintroapp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.selfintroapp.mapper")
 public class SelfintroAppApplication {
 
 	public static void main(String[] args) {
