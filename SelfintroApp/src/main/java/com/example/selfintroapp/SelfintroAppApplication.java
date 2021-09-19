d85ed38033ea77e561b920e51eb91450a969fec7
Keisuke-yunoki <59494597+Keisuke-yunoki@users.noreply.github.com>
2021-09-19 21:54:30 +0900

    application.yml変更。

--- SelfintroApp/src/main/java/com/example/selfintroapp/SelfintroAppApplication.java
+++ SelfintroApp/src/main/java/com/example/selfintroapp/SelfintroAppApplication.java
@@ -1,11 +1,9 @@
 package com.example.selfintroapp;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.selfintroapp.mapper")
 public class SelfintroAppApplication {
 
 	public static void main(String[] args) {
