d348e87473dec7f210374aaff24ffd0fd498304a
Keisuke-yunoki <59494597+Keisuke-yunoki@users.noreply.github.com>
2021-11-29 07:49:13 +0900

    pom.修正

--- SelfintroApp/src/main/java/com/example/selfintroapp/controller/SelfintroAppController.java
+++ SelfintroApp/src/main/java/com/example/selfintroapp/controller/SelfintroAppController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.selfintroapp.domain.Appdata;
 import com.example.selfintroapp.service.SelfintroAppService;
 
+
 @Controller
 @RequestMapping("/appdatas")
 public class SelfintroAppController {
